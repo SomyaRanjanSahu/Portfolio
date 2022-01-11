d05fde91c073642f7e7d74cef05c69dc3b822a4c
Somya_Sahu <somyasahu10@gmail.com>
2022-01-11 21:58:57 +0530

    Changed dark theme

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'pink');
+        document.documentElement.setAttribute('data-theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
